34735d155228be573a6b342e074353ee4c96b782
quan901 <1023112857@qq.com>
2022-04-05 20:07:13 +0800

    Add files via upload

---
+++ pages/index/index.js
@@ -0,0 +1,74 @@
+// index.js
+Page({
+
+  data:{
+      
+      list:[
+          {
+              id:0,
+              image:"../img/business/KFC.png",
+              openinghours:"",
+              name:"蜀七公甲鱼鸡子牛鞭煲（银丰店）",
+              price:"39",
+              locate:"银丰路",
+              class:"焖锅",
+            //   star:-11,
+              star:4.5,
+              label:["公益商家","银丰路火锅人气榜第1名"],
+              cheapimg:"../img/icon/cheap.png",
+              cheap:"双人餐39元,3人餐89元，4人餐138元",
+              cheaptap:"限时减29.1",
+              honor:"../img/icon/honor-high.png",
+              service:"",
+              distance:"1.2km",
+              discountimg:"../img/icon/discount.png",
+              discount:"88代100元",
+              timelogo:"../img/icon/clock.png"
+              
+          },
+          {
+              id:1,
+              image:"../img/business/McDonald.png",
+              openinghours:"11:00",
+              name:"是但茶餐厅（南城店）",
+              price:"27",
+              locate:"中心广场/华凯广场",
+              class:"茶餐厅",
+              star:3.5,
+              label:[],
+              cheapimg:"../img/icon/cheap.png",
+              cheap:"单人餐 18.8元起",
+              cheaptap:"新客减3",
+              honor:"../img/icon/honor-low.png",
+              service:"../img/icon/service-buy.png",
+              discountimg:"../img/icon/discount.png",
+              distance:"339m",
+              discount:"75代100元",
+              timelogo:"../img/icon/clock.png"
+          },
+          {
+              id:2,
+              image:"../img/business/CDC.png",
+              openinghours:"11:00",
+              name:"食代湘承（元美店）",
+              price:"47",
+              locate:"鸿福路口",
+              class:"湘菜",
+              star:4.5,
+              label:["鸿福路口地方菜系人气榜第5名"],
+              cheapimg:"../img/icon/cheap.png",
+              cheap:"双人餐78元",
+              cheaptap:"85代100元",
+              honor:"../img/icon/honor-low.png",
+              service:"../img/icon/service-book.png",
+              distance:"1.3km",
+              discountimg:"../img/icon/discount.png",
+              discount:"85代100元",
+              timelogo:"../img/icon/clock.png"
+          }
+      ]
+  }
+
+
+
+})
